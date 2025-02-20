fd0f6d2b09328cd6339c0fcd15ee4004e4aceac5
alazarZharegewayne <alazarharegewaynez@gmail.com>
2025-02-21 00:19:14 +0300

    part 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import WelcomeMessage from './WelcomeMessage';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <WelcomeMessage />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,8 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+    
     </>
   )
 }
 
-export default App
+export default App;
