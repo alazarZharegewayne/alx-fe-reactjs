98b8ee32ccd620ae06bedbea8a7808d0bcf6d550
alazarZharegewayne <alazarharegewaynez@gmail.com>
2025-03-09 19:57:05 +0300

    initial commit, formik

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,12 @@
+import FormikForm from "./components/formikForm";
+
+function App() {
+  return (
+    <div>
+      <h1>User Registration (Formik)</h1>
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
