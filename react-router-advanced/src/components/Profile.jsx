d72b57eaa0eccfd9c86872a6fc5bc4616d8ed3b5
alazarZharegewayne <alazarharegewaynez@gmail.com>
2025-03-09 20:36:56 +0300

    1

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,8 +1,30 @@
-import { useParams } from 'react-router-dom';
+import { Routes, Route, Link, useParams } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
 function Profile() {
   const { userId } = useParams();
-  return <div>User Profile for ID: {userId}</div>;
+
+  return (
+    <div>
+      <h1>Profile for User: {userId}</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="details">Profile Details</Link>
+          </li>
+          <li>
+            <Link to="settings">Profile Settings</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default Profile;
